d9f8a817f7f2e7bdfd2b3c65754e52a520acf858
Hussein Ismail <hussein.ismail@mail.com>
2016-03-26 15:33:57 +0200

    Change port and logging level. Add path to wkhtmltopdf tool.

--- run.bat
+++ run.bat
@@ -1,1 +1,3 @@
-python odoo.py --config .openerp-server.config --data-dir=DATA_DIR 
+@echo off
+set path=C:\Program Files\wkhtmltopdf\bin;%path%
+python odoo.py --config .openerp-server.config --data-dir=DATA_DIR --xmlrpc-port=8008 --log-level=debug
