19f8c797c6dfcb67c33e960bb1c867a2e063f64f
Hussein Ismail <hussein.ismail@mail.com>
2016-03-27 22:51:50 +0300

    Custom application/module.

---
+++ own-addons/todo_user/__openerp__.py
@@ -0,0 +1,6 @@
+{
+    'name': 'Multiuser To-Do',
+    'description': 'Extend To-Do app to multiuser.',
+    'author': 'Daniel Reis',
+    'depends': ['todo_app'],
+}
--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
 @echo off
 set path=C:\Program Files\wkhtmltopdf\bin;%path%
-python odoo.py --config .openerp-server.config --data-dir=DATA_DIR --xmlrpc-port=8008 --log-level=debug
+python odoo.py --config .openerp-server.config --data-dir=DATA_DIR --xmlrpc-port=8008 --log-level=debug -u todo_app
